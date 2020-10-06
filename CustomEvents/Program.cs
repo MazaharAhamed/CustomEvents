782cdc54dd2aba711af58d2a5e2ab03792ff3c3a
MazaharAhamed <mazaharahamed77@gmail.com>
2020-10-06 11:11:04 +0530

    Add project files.

---
+++ CustomEvents/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CustomEventsAndEventArgs
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            worker.DoWork(8, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked : " + e.Hours + " " + e.WorkType);
+        }
+        static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work is done");
+
+        }
+
+    }
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
